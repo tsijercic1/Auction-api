5912445fb993737a8acb7477daa4ef69ffeea59e
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-02-20 14:36:40 +0100

    Added routes for user product creation and getting

--- auction-api/src/main/java/com/github/tsijercic1/auctionapi/controllers/ProductController.java
+++ auction-api/src/main/java/com/github/tsijercic1/auctionapi/controllers/ProductController.java
@@ -6,21 +6,17 @@ import com.github.tsijercic1.auctionapi.models.Product;
 import com.github.tsijercic1.auctionapi.models.User;
 import com.github.tsijercic1.auctionapi.repositories.UserRepository;
 import com.github.tsijercic1.auctionapi.security.CurrentUser;
-import com.github.tsijercic1.auctionapi.security.JwtTokenProvider;
 import com.github.tsijercic1.auctionapi.security.UserPrincipal;
 import com.github.tsijercic1.auctionapi.services.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,7 +35,7 @@ public class ProductController {
 
     @GetMapping("/products")
     @PermitAll
-    public ResponseEntity<Page<Product>> getAll(Pageable pageable) {
+    public ResponseEntity<Iterable<Product>> getAll(Pageable pageable) {
         logger.info("Accessed route products");
         return ResponseEntity.ok(productService.getAll(pageable));
     }
@@ -47,16 +43,27 @@ public class ProductController {
     @GetMapping("/{username}/products")
     @RolesAllowed("USER")
     public ResponseEntity<List<Product>> getAllForUser(@PathVariable String username, @CurrentUser UserPrincipal userPrincipal) {
-        logger.info(username+" "+userPrincipal.getUsername());
+        logger.info(username + " " + userPrincipal.getUsername());
+        User user = checkAuthorization(username, userPrincipal);
+        return ResponseEntity.ok(productService.getAllForUser(user));
+    }
+
+    @PostMapping("/{username}/products")
+    @RolesAllowed("USER")
+    public ResponseEntity<Product> create(@PathVariable String username, @Valid @RequestBody Product product, @CurrentUser UserPrincipal userPrincipal) {
+        User user = checkAuthorization(username, userPrincipal);
+        product.setSeller(user);
+        return ResponseEntity.ok(productService.create(product));
+    }
+
+    private User checkAuthorization(@PathVariable String username, @CurrentUser UserPrincipal userPrincipal) {
         Optional<User> user = userRepository.findByUsername(username);
-        if(!user.isPresent()){
+        if (!user.isPresent()) {
             throw new ResourceNotFoundException("User", "username", username);
         }
         if (!user.get().getUsername().equals(userPrincipal.getUsername())) {
             throw new UnauthorizedException("Username is not yours");
         }
-        return ResponseEntity.ok(productService.getAllForUser(user.get()));
+        return user.get();
     }
-
-
 }
