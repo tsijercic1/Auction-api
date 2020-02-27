3fd6d01dcdea2a49a9e185fd7bd6a5456e276143
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-02-27 10:44:48 +0100

    removed client from branch

--- auction-client/src/app/shop/category-list/category-item/category.model.ts
+++
@@ -1,5 +0,0 @@
-export class Category {
-  constructor(public id, public name: string, public subcategories: Array<string>) {
-
-  }
-}
--- auction-client/src/app/user.model.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-  constructor(public id: string, public username: string, public token: string, public name: string, public surname: string,
-              public picture: string ) {
-  }
-}
